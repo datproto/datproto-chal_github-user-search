6cd093a1acf385d54063ef270f854f1ff5b17e7e
Dat Proto <contact@datproto.com>
2023-03-10 08:27:06 +0700

    RESEARCH: React Spring

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,11 +5,14 @@ import Header                        from '@/components/molecules/Header'
 import Search                        from '@/components/molecules/Search'
 import {useLazyGetGithubUserByNameQuery} from '@/reducers/githubApi'
 import Card                          from '@/components/molecules/Card'
+import {animated, useTrail} from '@react-spring/web'
 
 function Test() {
   const [githubUser, setGithubUser] = React.useState<string>('airbnb')
 
   const [getGithubUserByName, {data}] = useLazyGetGithubUserByNameQuery()
+
+  const [display, setDisplay] = React.useState<boolean>(false)
 
   const handleGithubSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // Prevents page reload on form submission
@@ -20,6 +23,22 @@ function Test() {
     setGithubUser(event.target.value)
   }
 
+
+  // Animation dots
+  const dots = ['.', '.', '.']
+  const loadingDots = useTrail(dots.length, {
+    from: {
+      y: 0
+    },
+    to: {
+      y: -10
+    },
+    config: {
+      duration: 1000
+    },
+    loop: true
+  })
+
   return (
     <main>
       <Header />
@@ -27,29 +46,49 @@ function Test() {
       <Search handleSubmit={handleGithubSearchSubmit} handleSearchTerm={handleGithubSearchTerm} />
 
       {data ? (
-        <Card
-          name={data.name}
-          github={{
-            name: data.login,
-            url: data.html_url
-          }}
-          avatar={data.avatar_url}
-          bio={data.bio}
-          indices={{
-            followers: data.followers,
-            following: data.following,
-            repos: data.public_repos
-          }}
-          personal_information={{
-            location: data.location,
-            blog: data.blog,
-            twitter: data.twitter_username,
-            joinDate: data.created_at
-          }}
-        />
+        <>
+          {display ? (
+            <Card
+              name={data.name}
+              github={{
+                name: data.login,
+                url: data.html_url
+              }}
+              avatar={data.avatar_url}
+              bio={data.bio}
+              indices={{
+                followers: data.followers,
+                following: data.following,
+                repos: data.public_repos
+              }}
+              personal_information={{
+                location: data.location,
+                blog: data.blog,
+                twitter: data.twitter_username,
+                joinDate: data.created_at
+              }}
+            />
+          ) : (
+            <div
+              className='flex w-full gap-6 rounded-2xl bg-github-white-main py-8 px-6 dark:bg-github-blue-yankees dark:text-white md:p-10 lg:p-12'
+            >
+              Getting user data {loadingDots.map((spring, key) => {
+                return (
+                  <animated.span key={key} style={{
+                    ...spring
+                  }}>
+                    {dots[key]}
+                  </animated.span>
+                )
+            })}
+            </div>
+          )}
+        </>
       ) : (
-        <div className='flex w-full gap-6 rounded-2xl bg-github-white-main py-8 px-6 dark:bg-github-blue-yankees md:p-10 lg:p-12'>
-          Please input a Github username above ...
+        <div
+          className='flex w-full gap-6 rounded-2xl bg-github-white-main py-8 px-6 dark:bg-github-blue-yankees dark:text-white md:p-10 lg:p-12'
+        >
+          Please input a Github username above to search for a user.
         </div>
       )}
     </main>
